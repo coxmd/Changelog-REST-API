f623a3191acdb3948e8c2134d229e63d74af3899
coxmd <coxmusyoki@gmail.com>
2022-11-23 23:43:21 +0300

    Implemented JWT tokens for authentication

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config(); // Looks at env file and gets all environment variables - Added to index file because it is the entry file to our server
+
 import app from "./server";
 
 app.listen(3001, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from "express";
-import router from './router';
+import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
+import { protect } from "./modules/auth";
 
 const app = express(); // Makes an api
+
+app.use(cors());
+app.use(morgan("dev")); // For logging
+app.use(express.json()); // Allows clients to send json
+app.use(express.urlencoded({ extended: true })); // Allows client to add query string parameters and decodes and encodes it properlly
 
 app.get("/", (req, res) => {
   console.log("hello from express");
@@ -9,6 +17,6 @@ app.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
-app.use('/api', router) // Allows you to aply some type of global configuration
+app.use("/api", protect, router); // Allows you to aply some type of global configuration
 
 export default app;
