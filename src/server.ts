56c21cae34b6632323e87e6ca1da0b021642ad84
coxmd <coxmusyoki@gmail.com>
2022-11-23 22:41:58 +0300

    Implemented routing structure

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import router from './router';
 
 const app = express(); // Makes an api
 
@@ -7,5 +8,7 @@ app.get("/", (req, res) => {
   res.status(200);
   res.json({ message: "hello" });
 });
+
+app.use('/api', router) // Allows you to aply some type of global configuration
 
 export default app;
